ac6adc36b1435cdbf6ac38fa3e10a02e1bf557fa
vrm <sotormd@proton.me>
2026-01-28 18:05:31 +0530

    fix: immutable users

--- configuration.nix
+++ configuration.nix
@@ -63,6 +63,11 @@
 
   users.mutableUsers = false;
 
+  security.sudo = {
+    enable = true;
+    wheelNeedsPassword = false;
+  };
+
   # List packages installed in system profile. To search, run:
   # $ nix search wget
   environment.systemPackages = with pkgs; [
