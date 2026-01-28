960be98b32354d333e01add0977ea82103f1b3e7
sotormd <sotormd@vm1>
2026-01-28 01:31:32 -0500

    feat: initial commit

---
+++ flake.nix
@@ -0,0 +1,17 @@
+{
+description = "vm1";
+
+inputs.nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+
+outputs = inputs: {
+
+nixosConfigurations.vm1 = inputs.nixpkgs.lib.nixosSystem {
+
+specialArgs = { inherit inputs; };
+modules = [ ./configuration.nix ];
+
+};
+
+};
+
+}
