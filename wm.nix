f548a544a9ec7c218c8117b7b3487631ea37e080
vrm <sotormd@proton.me>
2026-01-28 18:13:32 +0530

    feat: switch to labwc

--- wm.nix
+++ wm.nix
@@ -1,10 +1,3 @@
 {
-  services.xserver = {
-    enable = true;
-    displayManager.startx = {
-      enable = true;
-      generateScript = true;
-    };
-    windowManager.openbox.enable = true;
-  };
+  programs.labwc.enable = true;
 }
