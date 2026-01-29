2b28d9b3ab1295f6b2e6ce600f4e403c162b4afe
vrm <sotormd@proton.me>
2026-01-29 15:23:10 +0530

    feat: add nixosModule

--- flake.nix
+++ flake.nix
@@ -1,15 +1,19 @@
 {
-  description = "vm1";
+  description = "minimal openbox";
 
   inputs.nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
 
-  outputs = inputs: {
-    nixosConfigurations.vm1 = inputs.nixpkgs.lib.nixosSystem {
-      specialArgs = { inherit inputs; };
-      modules = [
-        ./configuration.nix
-        ./wm.nix
-      ];
+  outputs =
+    { self, ... }@inputs:
+    {
+      nixosConfigurations.vm1 = inputs.nixpkgs.lib.nixosSystem {
+        specialArgs = { inherit inputs; };
+        modules = [
+          ./configuration.nix
+          self.nixosModules.minimal-openbox
+        ];
+      };
+
+      nixosModules.minimal-openbox = _: { imports = [ ./wm.nix ]; };
     };
-  };
 }
