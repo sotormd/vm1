b2cefa9504845d265c14c3cc9683437a38b7bd02
vrm <sotormd@proton.me>
2026-01-29 10:46:27 +0530

    feat: add theme?

--- wm.nix
+++ wm.nix
@@ -10,7 +10,15 @@
     windowManager.openbox.enable = true;
   };
 
-  environment.systemPackages = [ pkgs.azeret-mono ];
+  environment.systemPackages = with pkgs; [
+    azeret-mono
+    obconf
+
+    (pkgs.runCommand "my-openbox-theme" { } ''
+      mkdir -p $out/share/themes
+      cp -r ${./dots/theme} $out/share/themes/
+    '')
+  ];
 
   environment.etc."X11/xinit/xinitrc".source = ./dots/xinitrc;
 
